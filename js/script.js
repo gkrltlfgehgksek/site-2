d71e87e64329d2e20fe0eb160936f389b24770a9
302kkk <leesb4397@naver.com>
2024-08-14 16:35:26 +0900

    8.14일

--- js/script.js
+++ js/script.js
@@ -1,26 +1,37 @@
-//ScrollGiveClass플러그인 호출
-
+//ScrollGiveClass 플러그인 호출
 var sgc1 = new ScrollGiveClass("header",{
-    baseline:'100'
+    baseline:100
 });
 
-//Swiper플러그인 호출
-
-const swiper = new Swiper('#main_view', {
+//swiper 플러그인 호출
+const swiper1 = new Swiper('#main_view', {
     // Optional parameters
-  wrapperClass:'slider',
-  slideClass:'view',
-
-  autoplay: { //자동슬라이드
-      delay: 5000, //하나의 슬라이드가 보여주는 시간
-      },
-      speed:800,//슬라이드가 전환되는(움직이는) 시간
-      spaceBetween:10, //슬라이드 사이간격
+    wrapperClass:'slider',
+    slideClass:'view',
+    autoplay: { //자동슬라이드
+      delay: 5000, //하나의 슬라이드를 보여주는 시간
+    },
+    speed:800, //슬라이드가 전환되는 시간
+    spaceBetween:10, //슬라이드 사이간격
     // If we need pagination
-    pagination: {
+    pagination: { 
       el: '.pager', //버튼영역 대상
-      bulletActiveClass: 'active', //활성화된 요소에 추가되는 클래스
+      bulletActiveClass:'active', //활성화된 요소에 추가되는 클래스
       clickable:true, //클릭시 슬라이드 이동
     },
-  
+         
   });
+  //press영역 스와이퍼 호출
+  const swiper2 = new Swiper("#press .inner", {
+    wrapperClass:'slider',
+    slideClass:'item',
+    slidesPerView: 5,
+    spaceBetween:30,
+    pagination:{
+      el:'.dot',
+      bulletActiveClass:'active',
+      clickable:true,
+    }
+  });
+
+  swiper2.slidesPerView = 4;
